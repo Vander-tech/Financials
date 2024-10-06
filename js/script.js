2a8fc056924dc8fea7d566f509de209aa7311c67
Vander-tech <vanderlee.frank@outlook.com>
2024-10-06 19:43:15 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Custom JS code can go here, such as form validation or interactive features
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+  });
